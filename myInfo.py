9d72c30268c024d9276258b3ed6a42639b3df543
antoniov289 <98064280+antoniov289@users.noreply.github.com>
2022-01-24 18:55:29 -0500

    myInfo version 2

--- myInfo.py
+++ myInfo.py
@@ -1,5 +1,7 @@
-#CS175-50
+#CS175L-50
 #Antonio Vinagre
 #myInfo
 print("My name is Antonio Vinagre")
-print("I am a student")
+print("I live at 289 Hudson Ave., Township of Washington, NJ 07676")
+print("My phone number is (201)419-4145")
+print("I am a math major with computer science and sociology minors.")

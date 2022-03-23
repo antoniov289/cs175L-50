0000eee969e90ad9fae189ad480b7ec5f6f5c804
antoniov289 <98064280+antoniov289@users.noreply.github.com>
2022-03-23 18:55:51 -0400

    average from input v2

--- average_from_input.py
+++ average_from_input.py
@@ -3,20 +3,40 @@
 #Spring 2022
 #average_from_input.py
 
-def main():
-    nums_file = open('numbers.txt', 'r')
-    #line = nums_file.readline()
-    tot = 0
-    count = 0
-    for line in nums_file:
-        num = float(line)
-        tot += num
-        count += 1
-        print(f"I've read in {count} number(s). Current number is: {num}. Total is: {tot}.")
+def get_values():
+    try:
+        nums_file = open('numbers.txt', 'r')
+    except IOError:
+        print('Error: Make sure the file is in the correct folder with the correct name.')
+        tot = 0
+        count = -1
+    else:
+        tot = 0
+        count = 0
+        for line in nums_file:
+            try:
+                num = float(line)
+            except ValueError:
+                print('Error: Some data was not in the form of a valid number and it was skipped.')
+            else:
+                tot += num
+                count += 1
+                print(f"I've read in {count} number(s). Current number is: {num}. Total is: {tot}.")
+        nums_file.close()
+    return tot, count
 
-    print(f"Average: {tot/count}")
-        
-    nums_file.close()
+def calc_avg(total, count):
+    avg = total/count
+    return avg
+
+def print_avg(average):
+    print(f"Average: {average}")
+
+def main():
+    tot, count = get_values()
+    average = calc_avg(tot, count)
+    if count != -1:
+        print_avg(average)
 
 if __name__ == '__main__':
     main()
